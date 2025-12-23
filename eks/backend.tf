4930d0415a18f193c69ccf024bcf0733b18462e2
nidaafreen-k <103402915+nidaafreen-k@users.noreply.github.com>
2025-12-23 21:50:10 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "dev-nida-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "state-locking-tf"
+    dynamodb_table = "state-locking"
     encrypt        = true
   }
 }
